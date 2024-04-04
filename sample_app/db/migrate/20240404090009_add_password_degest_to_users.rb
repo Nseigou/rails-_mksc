45e56c37c9238c7793222094ba7a4a2f5ecc8cad
Nseigou <nagano_1si@yahoo.co.jp>
2024-04-04 19:18:59 +0900

    finish-chapter6

---
+++ sample_app/db/migrate/20240404090009_add_password_degest_to_users.rb
@@ -0,0 +1,5 @@
+class AddPasswordDegestToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_03_053420) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_04_090009) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
